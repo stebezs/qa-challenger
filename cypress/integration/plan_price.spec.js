ad80c0ce5b35b94edbce01a748889fe09da27071
Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>
2020-04-11 18:10:13 -0300

    TU03-Plan price

---
+++ cypress/integration/plan_price.spec.js
@@ -0,0 +1,18 @@
+context('Planos', () => {
+
+    context('verify all "Planos" cases', () => {
+      beforeEach(() => {
+        cy.visit('https://getlabor.com.br')
+      })
+
+        it('contains "Planos" field', () => {
+          cy.get('[href="/planos"]').contains("Planos")
+        })
+
+    it('click on the button "entrar"', () => {
+      cy.visit('https://getlabor.com.br')
+      cy.contains('Planos').click({force: true})
+    })
+    
+  })
+})
