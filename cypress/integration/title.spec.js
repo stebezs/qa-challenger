e57d31156ee172573164651cd62e22a3db756fbe
Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>
2020-04-11 18:08:06 -0300

    TU01-Correct title

---
+++ cypress/integration/title.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Check the title', () => {
+  beforeEach(() => {
+    cy.visit('https://getlabor.com.br/')
+  })
+
+  it('contains "Labor - Controle de Horas"', () => {
+    cy.title().should('contain', 'Labor - Controle de Horas')
+  })
+  
+})
