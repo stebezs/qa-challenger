376d97f1361864766d17bcf5c9abe9bbb1c18d8c
Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>
2020-04-11 18:09:22 -0300

    TU02-Initial pricing

---
+++ cypress/integration/initial_price.spec.js
@@ -0,0 +1,13 @@
+const urls = ['https://getlabor.com.br/']
+
+describe('Teste o Labor por 30 dias gratuitamente!', () => {
+
+  beforeEach(() => {
+    cy.visit('https://getlabor.com.br/')
+  })
+
+  it('contains "Comece gratuitamente "', () => {
+    cy.get('[href="https://app.getlabor.com.br/cadastrar"]').contains("Comece Gratuitamente")
+  })
+  
+})
